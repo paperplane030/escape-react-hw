eef8469c8679570935bdf3715825fd41501131da
paperplane030 <dinosaur0023@gmail.com>
2022-03-19 02:21:05 +0800

    嘗試修正首頁路徑

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter>
       <Nav/>
       <Routes>
-        <Route path="escape-react-hw/" element={<Index />} />
+        <Route index element={<Index />} />
         <Route path="products" element={<Products />} />
         <Route path="products/:productId" element={<ProductDetail />} />
         <Route path="articles" element={<Article />} />
